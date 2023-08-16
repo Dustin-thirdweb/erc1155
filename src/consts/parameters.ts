0aee384a9782da18188ee75ccbbbf62d5cd589e3
Dustin <135719141+Dustin-thirdweb@users.noreply.github.com>
2023-08-16 16:31:59 -0500

    Add files via upload

---
+++ src/consts/parameters.ts
@@ -0,0 +1,28 @@
+/** Change these values to configure the application for your own use. **/
+
+// Your smart contract address (available on the thirdweb dashboard)
+// For existing collections: import your existing contracts on the dashboard: https://thirdweb.com/dashboard
+export const contractConst = "";
+
+export const tokenIdConst = "";
+
+// The name of the chain your contract is deployed to.
+// Provide a string for standard chains, or import one from @thirdweb-dev/chains package.
+// Learn more: https://portal.thirdweb.com/react/react.thirdwebprovider#activechain-recommended
+export const chainConst = "";
+
+// Configure the primary color for buttons and other UI elements
+export const primaryColorConst = "";
+
+// Choose between "light" and "dark" mode
+export const themeConst = "dark";
+
+// Gasless relayer configuration options
+export const relayerUrlConst = ""; // OpenZeppelin relayer URL
+export const biconomyApiKeyConst = ""; // Biconomy API key
+export const biconomyApiIdConst = ""; // Biconomy API ID
+export const clientIdConst = ""; // Client ID
+
+// Wallet connect configuration options
+export const paperClientIdConst = "f4d2fc79-523f-4b45-ba1c-32ffa8a53872"; // Paper wallet client ID
+export const magicLinkApiKeyConst = "pk_live_5BD2E64C36DC78AA"; // Magic link API key
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,73 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-import { getChainBySlug } from "@thirdweb-dev/chains";
+import {
+  ThirdwebProvider,
+  ConnectWallet,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+  paperWallet,
+  localWallet,
+  trustWallet,
+  magicLink,
+  rainbowWallet,
+} from "@thirdweb-dev/react";
 import "./styles/globals.css";
-
-const urlParams = new URL(window.location.toString()).searchParams;
-const network = urlParams.get("network") || "ethereum";
-const activeChain = getChainBySlug(network);
+import { Toaster } from "./components/ui/Toaster";
+import { getGasless } from "./utils/getGasless";
+import {
+  biconomyApiIdConst,
+  biconomyApiKeyConst,
+  chainConst,
+  relayerUrlConst,
+  clientIdConst,
+  paperClientIdConst,
+  magicLinkApiKeyConst,
+} from "./consts/parameters";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
+const urlParams = new URL(window.location.toString()).searchParams;
+
+const relayerUrl = urlParams.get("relayUrl") || relayerUrlConst || "";
+const biconomyApiKey =
+  urlParams.get("biconomyApiKey") || biconomyApiKeyConst || "";
+const biconomyApiId =
+  urlParams.get("biconomyApiId") || biconomyApiIdConst || "";
+const sdkOptions = getGasless(relayerUrl, biconomyApiKey, biconomyApiId);
+
+const chain = (urlParams.get("chain") && urlParams.get("chain")?.startsWith("{")) ? JSON.parse(String(urlParams.get("chain"))) : urlParams.get("chain") || chainConst;
+
+const clientId = urlParams.get("clientId") || clientIdConst || "";
+
+const paperClientId = urlParams.get("paperClientId") || paperClientIdConst ||"";
+
+const magicLinkApiKey = urlParams.get("magicLinkApiKey") || magicLinkApiKeyConst ||"";
+
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider activeChain={activeChain}>
+  <ThirdwebProvider
+    clientId={clientId}
+    activeChain={chain} 
+    sdkOptions={sdkOptions}
+    supportedWallets={[
+      metamaskWallet(),
+      coinbaseWallet(),
+      walletConnect(),
+      localWallet(),
+      trustWallet(),
+      paperWallet({
+        paperClientId: paperClientId,
+      }),
+      magicLink({
+        apiKey: magicLinkApiKey,
+      }),
+      rainbowWallet(),
+    ]}
+  >
+      <Toaster />
       <App />
     </ThirdwebProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
